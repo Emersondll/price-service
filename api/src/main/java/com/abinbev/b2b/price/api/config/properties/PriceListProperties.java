c9c4a9ecfe189a2cb757031733ab139561717122
Emerson Lima <emersondll@outlook.com>
2026-03-21 08:00:49 -0300

    Commit automático do arquivo api/src/main/java/com/abinbev/b2b/price/api/config/properties/PriceListProperties.java

---
+++ api/src/main/java/com/abinbev/b2b/price/api/config/properties/PriceListProperties.java
@@ -0,0 +1,21 @@
+package com.abinbev.b2b.price.api.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("pricelistpriority")
+public class PriceListProperties {
+
+	private String countriesEnabled;
+
+	public String getCountriesEnabled() {
+
+		return countriesEnabled;
+	}
+
+	public void setCountriesEnabled(final String countriesEnabled) {
+
+		this.countriesEnabled = countriesEnabled;
+	}
+}
