2fb24dca3d166cf6ef5765f471e9f409915fa0c7
Emerson Lima <emersondll@outlook.com>
2026-03-25 15:08:48 -0300

    Commit automático do arquivo api/src/test/java/com/abinbev/b2b/price/api/testhelpers/ArchUnitConstants.java

---
+++ api/src/test/java/com/abinbev/b2b/price/api/testhelpers/ArchUnitConstants.java
@@ -0,0 +1,33 @@
+package com.abinbev.b2b.price.api.testhelpers;
+
+public class ArchUnitConstants {
+
+	public static final String ALL_CLASSES_PACKAGE = "com.abinbev.b2b.price.api";
+
+	public static final String CONTROLLER_LAYER_NAME = "Controllers";
+	public static final String CONTROLLER_PACKAGE_PATH = "com.abinbev.b2b.price.api.controllers..";
+	public static final String CONTROLLERS_PACKAGE = "..controllers..";
+	public static final String CONTROLLER_SUFFIX = "Controller";
+
+	public static final String SERVICE_LAYER_NAME = "Services";
+	public static final String SERVICE_LAYER_PATH = "com.abinbev.b2b.price.api.services..";
+	public static final String SERVICE_PACKAGE = "..services..";
+	public static final String SERVICE_SUFFIX = "Service";
+
+	public static final String REPOSITORY_PACKAGE = "..repository..";
+	public static final String REPOSITORY_SUFFIX = "Repository";
+	public static final String REPOSITORY_LAYER_NAME = "Repository";
+	public static final String REPOSITORY_PACKAGE_PATH = "com.abinbev.b2b.price.api.repository..";
+
+	public static final String CONVERTER_PACKAGE = "..converters..";
+	public static final String CONFIG_PACKAGE_PATH = "com.abinbev.b2b.price.api.config";
+	public static final String CONFIG_SUFFIX = "Config";
+
+	public static final String MAIN_PACKAGE = "com.abinbev.b2b.price.api";
+	public static final String TEST_SUFFIX = "Test";
+	public static final String PACT_TEST_SUFFIX = "PactTest";
+	public static final String INTEGRATION_TEST_NAME = "Integration";
+	public static final String INTEGRATION_TEST_PACKAGE_PATH = "..integrations..";
+	public static final String ARCH_UNIT_PACKAGE_PATH = "com.abinbev.b2b.price.api.arch";
+	public static final String CONFIG_TEST_NAME = "Config";
+}
